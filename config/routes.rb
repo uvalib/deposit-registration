e19bdbe12fc0b9a7d1e39e0cd59a577632fd5d1d
UVADave <dpg3k@virginia.edu>
2016-07-14 15:50:13 -0400

    LIBRA-381: add version endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   resources :healthcheck, only: [ :index ]
+  resources :version, only: [ :index ]
 
   # You can have the root of your site routed with "root"
   root 'registrations#new'
