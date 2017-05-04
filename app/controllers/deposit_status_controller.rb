ee1e68b2303e4a5b3ed598a719b74d69d5d6264a
UVADave <dpg3k@virginia.edu>
2017-05-04 12:39:14 -0400

    Add search by created and exported dates

--- app/controllers/deposit_status_controller.rb
+++ app/controllers/deposit_status_controller.rb
@@ -2,18 +2,15 @@ class DepositStatusController < ApplicationController
 
   def index
 
-    @lookup = params[:lookup]
-    @deposits = []
-    @title = 'Libra Deposit Status'
-    status = :ok
+    # check inbound params
+    @cid_lookup, @created_lookup, @exported_lookup = pick_search_params
 
-    # are we doing a lookup
-    if @lookup.blank? == false
-      status, details = DepositStatus.get_status( @lookup )
-      if DepositStatus.status_ok?( status ) == true
-        @deposits = details
-      end
-    end
+    @title = 'Libra Deposit Status'
+    @deposits = []
+
+    # do the lookup
+    status, details = do_lookup( @cid_lookup, @created_lookup, @exported_lookup )
+    @deposits = details if DepositStatus.status_ok?( status )
 
     respond_to do |format|
       format.json { render json: @deposits, status: status }
@@ -26,4 +23,28 @@ class DepositStatusController < ApplicationController
     end
   end
 
+  private
+
+  def do_lookup( cid, created, exported )
+
+    # lookup by CID
+    return( DepositStatus.status_by_cid( cid ) ) if cid.present?
+
+    # lookup by created
+    return( DepositStatus.status_by_created( created ) ) if created.present?
+
+    # lookup by exported
+    return( DepositStatus.status_by_exported( exported ) ) if exported.present?
+
+    # nothing to do
+    return true, []
+  end
+
+  def pick_search_params
+
+    return params[:cid_lookup], nil, nil if params[:cid_lookup].present?
+    return nil, params[:created_lookup], nil if params[:created_lookup].present?
+    return nil, nil, params[:exported_lookup] if params[:exported_lookup].present?
+    return nil, nil, nil
+  end
 end
--- app/models/deposit_status.rb
+++ app/models/deposit_status.rb
@@ -1,35 +1,48 @@
 class DepositStatus
 
-	# Adds status check behavior
-	include StatusBehavior
+   # Adds status check behavior
+   include StatusBehavior
 
-	def self.get_status( computing_id )
+   def self.status_by_cid( computing_id )
+     url = "#{DEPOSITAUTH_URL}/?auth=#{API_TOKEN}&cid=#{computing_id}"
+     return( self.get_status( url ) )
+   end
 
-		begin
-			url = "#{DEPOSITAUTH_URL}/?auth=#{API_TOKEN}&cid=#{computing_id}"
-			#puts "==> #{url}"
-			response = HTTParty.get( url )
-			return response.code, response['details'] if self.status_ok?( response.code )
-			return response.code, response.message
-		rescue => e
-			return 500, e.message
-		end
+   def self.status_by_created( created )
+     url = "#{DEPOSITAUTH_URL}/?auth=#{API_TOKEN}&created=#{created}"
+     return( self.get_status( url ) )
+   end
 
-	end
+   def self.status_by_exported( exported )
+     url = "#{DEPOSITAUTH_URL}/?auth=#{API_TOKEN}&exported=#{exported}"
+     return( self.get_status( url ) )
+   end
 
-	def self.check_depositauth_endpoint
-		begin
-			url = "#{DEPOSITAUTH_URL}/healthcheck"
-			#puts "==> #{url}"
-			response = HTTParty.get( url )
-			if self.status_ok?( response.code )
-				return true, ''
-			else
-				return false, "Endpoint returns #{response.code}"
-			end
-		rescue => ex
-			return false, "Endpoint returns #{ex}"
-		end
-	end
+   def self.check_depositauth_endpoint
+      begin
+         url = "#{DEPOSITAUTH_URL}/healthcheck"
+         #puts "==> #{url}"
+         response = HTTParty.get( url )
+         if self.status_ok?( response.code )
+            return true, ''
+         else
+            return false, "Endpoint returns #{response.code}"
+         end
+      rescue => ex
+         return false, "Endpoint returns #{ex}"
+      end
+   end
+
+  def self.get_status( url )
+
+      #puts "==> #{url}"
+      begin
+         response = HTTParty.get( url )
+         return response.code, response['details'] if self.status_ok?( response.code )
+         return response.code, response.message
+      rescue => e
+         return 500, e.message
+      end
+   end
 
 end
