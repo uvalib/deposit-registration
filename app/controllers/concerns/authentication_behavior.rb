4342c528bb2d11355784b4de090876fc598d981f
UVADave <dpg3k@virginia.edu>
2017-11-16 13:48:58 -0500

    Enable query by create date and export date for admins. Add some helper text

--- app/controllers/concerns/authentication_behavior.rb
+++ app/controllers/concerns/authentication_behavior.rb
@@ -7,20 +7,25 @@ module AuthenticationBehavior
 
 	private
 
-	def current_user
-		return ENV['HTTP_REMOTE_USER'] if Rails.env == 'development'
-		return request.env['HTTP_REMOTE_USER']
-	end
-
 	def require_auth
 
     #
     # if we cannot get the user, then raise an exception...
     #
-		if current_user.blank?
+
+    @current_user = current_user( )
+    if @current_user.blank?
 			raise ActionController::RoutingError.new( 'Forbidden' )
 		end
 
 	end
+
+  #
+  # determine tyhe current user
+  #
+  def current_user
+    return ENV['HTTP_REMOTE_USER'] if Rails.env == 'development'
+    return request.env['HTTP_REMOTE_USER']
+  end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  #
+  # is the current user an admin
+  #
+  def user_is_admin?
+    #return false if @current_user.blank?
+    #return true if ['dpg3k', 'ecr2c', 'sah', 'naw4t'].include? @current_user
+    return false
+  end
+
 end
