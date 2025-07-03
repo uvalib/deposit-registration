c0e4a7fee04bdfba82e0939674b2ca2288517678
Nestor Walker <naw4t@virginia.edu>
2025-07-03 12:01:12 -0400

    Add gqp6qh to admin list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
   #
   def user_is_admin?
     return false if @current_user.blank?
-    return true if ['dpg3k', 'sah', 'naw4t', 'arb5w', 'wkb5j'].include? @current_user
+    return true if ['dpg3k', 'sah', 'naw4t', 'arb5w', 'wkb5j', 'gqp6qh'].include? @current_user
     return false
   end
 
